b24eff8d52b294841a5b294ad714d6e2445f4c63
Steve B <stvblngr@gmail.com>
2014-10-12 23:04:09 -0400

    travail sur index.php et ajout de bootstraptweak.css
    juste du travail sur le layout

--- index.php
+++ index.php
@@ -5,6 +5,26 @@ $user = new User();
 
 ?>
 
+    <!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap 101 Template</title>
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstraptweak.css" rel="stylesheet">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
 <?php
 
 if(Session::exists('home')) {
@@ -30,6 +50,21 @@ if($user->isLoggedIn()) {
 	<?php
 	}
 
-} else {
-	echo 'You need to <a href="login.php">log in</a> or <a href="register.php">register</a>!';
-}
+} else { ?>
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Logiciel</h1>
+        <p class="lead">Ambiosol<br> <?php echo 'Vous devez <a href="login.php">log in</a> ou <a href="register.php">créer un compte</a>'; ?></p>
+      </div>
+
+    </div><!-- /.container -->
+<?php }
+
+?>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
